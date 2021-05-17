168be77d0d7f36cfc48d7306325bd4b18a112c95
alsdream <AlsdnWorks@gmail.com>
2021-05-17 20:19:27 +0900

    floodfill

--- FloodFIll_Recursion/FloodFIll_Recursion/FloodFIll_Recursion.cpp
+++ FloodFIll_Recursion/FloodFIll_Recursion/FloodFIll_Recursion.cpp
@@ -1,8 +1,8 @@
-﻿// FloodFIll_Recursion.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
 #include <cstdlib>
-#include <iostream>
+#include<ctime>
 #include <vector>
+#include<stack>
 using namespace std;
 
 template<class T>
@@ -11,80 +11,94 @@ public:
 	WBcheck() {};
 	~WBcheck() {};
 	void init();
-	void display();
+	void findWhite_ctrl();
 	void findWhite(int x, int y);
-	void result();
+	bool result();
 
 private:
-	const bool w=true, b=false; 
-	int i=0, j=0, k=0;
+	const bool w = true, b = false;
+	const short pass = 2;
+
+	int i = 0, j = 0, k = 0;
+
 	int count[64];
-	int block[9][9];
-	const short pass=2;
+	int block[8][8];
 };
 
 
 template<class T>
 void WBcheck<T>::init() {
+	srand((unsigned int)time(NULL));
+	cout << "Created pattern is:" << endl;
 	for (i = 0; i <= 7; i++) {
 		for (j = 0; j <= 7; j++) {
 			if (rand() % 2 == 0) block[i][j] = b;
 			else block[i][j] = w;
-		}
-	}
-	display();
-}
-//최종본에서는 init와 display를 합칠것
-template<class T>
-void WBcheck<T>::display() {
-	for (i = 0; i <= 7; i++) {
-		for (j = 0; j <= 7; j++) {
-			cout << (int)block[i][j]<<" ";
+			cout << (int)block[i][j] << " ";
 		}
 		cout << endl;
 	}
-	
+}
+
+template<class T>
+void WBcheck<T>::findWhite_ctrl() {
 	for (i = 0; i <= 7; i++) {
-		for (j = 0; j <= 7; j++) {	
-			count[k]=0;
-			findWhite(i,j);
+		for (j = 0; j <= 7; j++) {
+			count[k] = 0;
+			findWhite(i, j);
 			k++;
 		}
 	}
-	
 }
 
 template<class T>
-void WBcheck<T>::findWhite(int x, int y){
-	if (block[x][y]==w){
-		block[x][y]=pass;
+void WBcheck<T>::findWhite(int x, int y) {
+	if (block[x][y] == w) {
+		block[x][y] = pass;
 		count[k]++;
-		if (x+1!=8)findWhite(x+1,y);	
-		if (y+1!=8)findWhite(x,y+1);
-		if (x-1!=-1)findWhite(x-1,y);
-		if (y-1!=-1)findWhite(x,y-1);
+		if (x + 1 != 8)findWhite(x + 1, y);
+		if (y + 1 != 8)findWhite(x, y + 1);
+		if (x - 1 != -1)findWhite(x - 1, y);
+		if (y - 1 != -1)findWhite(x, y - 1);
 	}
 }
 
 template<class T>
-void WBcheck<T>::result(){
-	int k=0;
-	while(k<=64){
-		if (count[k]>0){
-			cout << count[k]<<endl;
+bool WBcheck<T>::result() {
+	int k = 0, i = 0;
+	int size = 0;
+	stack <int> res;
+
+	while (k <= 63) {
+		if (count[k] > 0) {
+			res.push(count[k]);
+			size++;
 		}
 		k++;
 	}
 	
+	cout << size << " white areas of ";
+	while (i <= (size - 2)) {
+		cout << res.top() << ", ";
+		res.pop();
+		i++;
+	}
+	cout <<"and "<< res.top() << " cells";
+	res.pop();
+
+	if (res.empty() == false) return 1;
 }
 
-int main()
-{
+int main(){
 	WBcheck<void> opr;
 
+	cout << "1 as Whitecells, 0 as Blackcells" << endl;
 	opr.init();
+	cout << endl;
+	opr.findWhite_ctrl();
+	cout << endl;
 	opr.result();
-    std::cout << "Hello World!\n";
+	cout << endl;
 	return 0;
 }
 
---
+++ FloodFIll_Recursion/FloodFIll_Recursion/STACKCLAC_201721565.cpp
@@ -0,0 +1,111 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+template<class T>
+class StackCalc {
+public:
+	StackCalc() {};
+	~StackCalc() {};
+	void addProcess();
+	void aPushReverse(__int64 num);
+	void bPushReverse(__int64 num);
+	void printPop();
+
+private:
+	stack<int>m_a;
+	stack<int>m_b;
+	stack<int>a;
+	stack<int>b;
+	stack<int>result;
+};
+
+
+template<class T>
+void StackCalc<T>::bPushReverse(__int64 num) {
+	//뒤집혀서 계산되는오류, 넣은값을 다시 스택하면 값역시 다시 뒤집혀 원하는 값이 나온다.
+	while (num != 0){
+		m_b.push(num % 10);
+		num /= 10;
+	}
+	while (!m_b.empty()) {
+		b.push(m_b.top());
+		m_b.pop();
+	}
+}
+
+
+template<class T>
+void StackCalc<T>::aPushReverse(__int64 num) {
+	while (num != 0){
+		m_a.push(num % 10);
+		num /= 10;
+	}
+	while (!m_a.empty()) {
+		a.push(m_a.top());
+		m_a.pop();
+	}
+}
+
+
+template<class T>
+void StackCalc<T>::addProcess() {
+	int carry=0;
+	int addElemet;
+
+	while (a.empty() == false || b.empty() == false) {
+		//자리수맞추기용
+		if (a.empty() == true)a.push(0);
+		if (b.empty() == true)b.push(0);
+		addElemet = a.top() + b.top() + carry;
+		a.pop();
+		b.pop();
+		carry = 0;
+		if ((addElemet / 10) == true) {
+			carry = 1;
+			if (a.empty() == false || b.empty() == false) {
+				addElemet = addElemet - 10;
+			}
+		}
+		result.push(addElemet);
+	}
+}
+
+
+template<class T>
+void StackCalc<T>::printPop() {
+	int digit;
+
+	cout << "Result: ";
+	if (result.empty() == true)
+		cout << "0";
+	while (result.empty() == false) {
+		digit = result.top();
+		cout << digit;
+		result.pop();
+	}
+}
+
+
+int main() {
+	bool hello = 0;
+initialize:
+	StackCalc<int> opr;
+	__int64 number_a, number_b;
+	bool re = 0;
+	if (hello == 0) {
+		cout << "-Stack을 이용한 정수간 덧셈 계산기 201721565손민우-\n";
+		cout << "각 오퍼랜드값은 __int64값을 넘을 수 없습니다. 단, 결과가 범위를 넘는것은 가능합니다.\n";
+		cout << "carry가 없는 차를 계산 할 수 있습니다.\n\n";
+		hello=true;
+	}
+	cout << "operand 1: "; cin >> number_a;
+	opr.aPushReverse(number_a);
+	cout << "operand 2: "; cin >> number_b;
+	opr.bPushReverse(number_b);
+	opr.addProcess();
+	opr.printPop();
+	cout << "\n\nRetry ? \n 1 to yes\n" ; cin >> re;
+	if (re == 1)  goto initialize;
+	return 0;
+}
