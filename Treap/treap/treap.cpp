cf74d13b75f94ba5081d0f7f562e8d9f86a6c2bc
alsdream <AlsdnWorks@gmail.com>
2021-06-01 01:07:15 +0900

    형변환백업

--- Treap/treap/treap.cpp
+++ Treap/treap/treap.cpp
@@ -4,6 +4,31 @@
 #include <algorithm>
 using namespace std;
 
+template<class T>
+class Node {
+public:
+    Node() {
+        left = right = NULL;
+    }
+    Node(const T& val, const double& pri, Node* l = NULL, Node* r = NULL) {
+        value = val;
+        priorty = pri;
+        left = l;
+        right = r;
+    }
+    ~Node() {};
+    T value;
+    double priorty;
+    Node*left , * right;
+    Node* root;
+    bool setRoot(string val, double prior);
+    bool insertNode(string value, double priorty, Node* refer);
+    bool processData();
+private:
+
+   };
+
+
 typedef struct context {
     string name;
     int num_of_confirmed;
@@ -12,17 +37,9 @@ typedef struct context {
 }context;
 vector<context>list;
 
-typedef struct Node {
-    string value=NULL;
-    double priorty=0;
-    struct Node* left;
-    struct Node* right;
-}Node;
-Node* root;
-
-
-bool setRoot(string val, double prior) {
-    Node *newNode = (Node*)malloc(sizeof(Node));
+template<class T>
+bool Node<T>:: setRoot(string val, double prior) {
+    Node *newNode =new Node;
     newNode->priorty = prior;
     newNode->value=val;
     newNode->left;
@@ -31,11 +48,11 @@ bool setRoot(string val, double prior) {
     return true;
 }
 
-
-bool insertNode(string value, double priorty, Node* refer) {
-    if (0 > value.compare(refer->value)) {
+template<class T>
+bool Node<T>::insertNode(string value, double priorty, Node* refer) {
+    if (0 < value.compare(refer->value)) {
         if (refer->right == NULL) {
-            Node* newNode = (Node*)malloc(sizeof(Node));
+            Node* newNode = new Node;
             refer->right = newNode;
             newNode->value = value;
             newNode->priorty = priorty;
@@ -43,9 +60,9 @@ bool insertNode(string value, double priorty, Node* refer) {
         }
         else insertNode(value, priorty, refer->right);
     }
-    if (0 < value.compare(refer->value)) {
+    if (0 > value.compare(refer->value)) {
         if (refer->left == NULL) {
-            Node* newNode = (Node*)malloc(sizeof(Node));
+            Node* newNode = new Node;
             refer->left = newNode;
             newNode->value = value;
             newNode->priorty = priorty;
@@ -62,30 +79,34 @@ bool insertNode(string value, double priorty, Node* refer) {
 bool Arg_Data(const context& a, const context& b) {
     return a.rate_of_death > b.rate_of_death;
 }
-bool processData() {
+
+template<class T>
+bool Node<T>::processData() {
     sort(list.begin(), list.end(), Arg_Data);
     setRoot(list[0].name, list[0].rate_of_death);
+    cout << "다음데이터를 Treap처리합니다 " << "\n" << list[0].name << "\n" << list[0].num_of_confirmed << "\n" << list[0].num_of_death << "\n" << list[0].rate_of_death << "\n" << "\n" << endl;
+
     for (int i = 1; i < list.size(); i++) {
         insertNode(list[i].name, list[i].rate_of_death,root);
-        cout <<"다음데이터 처리"<< list[i].name<<"\n" << list[i].num_of_confirmed<<"\n" << list[i].num_of_death<<"\n" << list[i].rate_of_death<<"\n" << "\n" << endl;
+        cout <<"다음데이터를 Treap처리합니다 " << "\n" << list[i].name<<"\n" << list[i].num_of_confirmed<<"\n" << list[i].num_of_death<<"\n" << list[i].rate_of_death<<"\n" << "\n" << endl;
     }
     return true;
 }
-bool InsertData() {
-    
-}
+//bool InsertData() {
+//    
+//}
+
 int main() {
-    // InitRoot();
-    list.push_back({ "Algeria", 126860, 3418 });
-    list.push_back({ "Romania", 1075773, 29977 });
-    list.push_back({ "Russia", 5001505, 118482 });
-    list.push_back({ "San_Marino", 5089, 90 });
-    list.push_back({ "Serbia", 709939, 6777 });
-    list.push_back({ "Slovakia", 772833, 12296 });
-    list.push_back({ "Slovenia", 251773, 4675 });
-    list.push_back({ "Spain", 3647520, 79711 });
-    list.push_back({ "Sweden", 1063183, 14396 });
-    processData();
+    Node <string> nodedata;
+
+    list.push_back({ "Ftest", 100, 75 });
+    list.push_back({ "Atest", 100, 55 });
+    list.push_back({ "Dtest", 100, 60 });
+    list.push_back({ "Ctest", 100, 70 });
+    list.push_back({ "Htest", 100, 14 });
+    list.push_back({ "Jtest", 100, 45 });
+    list.push_back({ "Itest", 100, 65 });    
+    nodedata.processData();
     
     return 0;
 }
